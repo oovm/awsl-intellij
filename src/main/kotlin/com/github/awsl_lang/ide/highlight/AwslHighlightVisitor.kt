199ee3628551c9337fe02e0fb02e4fcce256f23d
Aster <galaster@foxmail.com>
2019-11-05 14:25:53 +0800

    🎀 Support highlight for `bind`, `on`

--- src/main/kotlin/com/github/awsl_lang/ide/highlight/AwslHighlightVisitor.kt
+++ src/main/kotlin/com/github/awsl_lang/ide/highlight/AwslHighlightVisitor.kt
@@ -3,6 +3,7 @@ package com.github.awsl_lang.ide.highlight
 import com.github.awsl_lang.ide.file_types.AwslFile
 import com.github.awsl_lang.ide.highlight.AwslHighlightColor.*
 import com.github.awsl_lang.language.psi.AwslHtmlKey
+import com.github.awsl_lang.language.psi.AwslHtmlKv
 import com.github.awsl_lang.language.psi.AwslTypes
 import com.github.awsl_lang.language.psi.AwslVisitor
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
@@ -12,6 +13,7 @@ import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.elementType
+import com.intellij.util.containers.headTail
 
 class AwslHighlightVisitor : AwslVisitor(), HighlightVisitor {
     private var infoHolder: HighlightInfoHolder? = null
@@ -25,30 +27,18 @@ class AwslHighlightVisitor : AwslVisitor(), HighlightVisitor {
     }
 
     override fun visitHtmlKey(o: AwslHtmlKey) {
-        val head = o.firstChild;
+        val sym = o.symbols.headTail();
+        val head = sym.first;
+        val rest = sym.second;
         when (head.text) {
-            "style" -> {
-                val symbols = mutableListOf<PsiElement>()
-                var resolveMode = true;
-                var e = head.nextSibling;
-                while (e != null) {
-                    when (e.elementType) {
-                        AwslTypes.SYMBOL -> symbols.add(e)
-                    }
-                    e = e.nextSibling
-                }
-                when {
-                    resolveMode -> {
-                        highlight(head, KEYWORD)
-                        symbols.map {
-                            highlight(it, METADATA)
-                        }
-                    }
-                    else -> highlight(o, SYM_PROP)
-                }
+            "style", "on", "bind", "if", "class" -> {
+                highlight(head, KEYWORD)
             }
             // attr = "?"
-            else -> highlight(o, SYM_PROP)
+            else -> highlight(head, SYM_PROP)
+        }
+        rest.map {
+            highlight(it, METADATA)
         }
     }
 
@@ -71,23 +61,24 @@ class AwslHighlightVisitor : AwslVisitor(), HighlightVisitor {
     override fun visit(element: PsiElement) = element.accept(this)
 }
 
-fun AwslHtmlKey.symbols(): List<PsiElement> {
-    val head = this.firstChild
-    val symbols = mutableListOf(head)
-    var e = head.nextSibling
-    while (e != null) {
-        when (e.elementType) {
-            AwslTypes.SYMBOL -> symbols.add(e)
+val AwslHtmlKey.symbols: List<PsiElement>
+    get() {
+        val head = this.firstChild
+        val symbols = mutableListOf(head)
+        var e = head.nextSibling
+        while (e != null) {
+            when (e.elementType) {
+                AwslTypes.SYMBOL -> symbols.add(e)
+            }
+            e = e.nextSibling
         }
-        e = e.nextSibling
+        return symbols
     }
-    return symbols
+
+fun AwslHtmlKv.isStyleMode(): Boolean {
+    return this.htmlKey.firstChild.text == "style"
 }
 
-//
-//fun AwslHtmlKv.isStyleMode() : Boolean {
-//    val head = this.htmlKey.firstChild
-//
-//
-//
-//}
+fun AwslHtmlKv.isEventMode(): Boolean {
+    return this.htmlKey.firstChild.text == "on"
+}
